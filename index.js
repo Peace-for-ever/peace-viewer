ae91ba8047f02315cda4abfc822dce4d3489d028
JM REMEUR <jm@remeur.fr>
2020-12-17 11:32:05 +0100

    connect kafka and forward messages with websockets

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const app = express()
+const expressWs = require('express-ws')(app);
+const eventsWs = expressWs.getWss('/');
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+	brokers: [process.env.kafkaHost || 'localhost:9092']
+});
+
+const consumer = kafka.consumer({ groupId: 'default' })
 
 app.use(express.static('public'));
+
+app.ws('/events', function(ws, req) {
+
+});
+
+setInterval(() => {
+	eventsWs.clients.forEach((ws) => {
+		ws.send('Hello !');
+	})
+}, 1000);
+
+const run = async () => {
+	await consumer.connect();
+	await consumer.subscribe({ topic: process.env.kafkaTopic || 'records', fromBeginning: false });
+
+	await consumer.run({
+		eachBatch: async ({ batch }) => {
+			eventsWs.clients.forEach((ws) => {
+				ws.send(batch.messages);
+			});
+		}
+	})
+};
+
+run().catch(console.error);
  
 app.listen(process.env.PORT || 3000)
